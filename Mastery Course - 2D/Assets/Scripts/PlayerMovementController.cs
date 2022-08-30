6e841a50006efdd71aa18ff9c884242b04867852
pinzonep <pap911@gmail.com>
2022-08-30 17:43:21 +1000

    Scripting introduction, movement

---
+++ Mastery Course - 2D/Assets/Scripts/PlayerMovementController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class PlayerMovementController : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed = 4;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal"); // gives a value back between -1 and 1
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontal, vertical); // gives direction
+
+        transform.position += movement * Time.deltaTime * moveSpeed; // move character around - access transform in the Inspector
+    }
+}
